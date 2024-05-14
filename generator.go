e0ca17614f191f13d52d8ca348c95184b2556bff
Brecht Van der Elst <brechtvanderelst@gmail.com>
2024-05-14 23:41:31 +0200

    POC

--- generator.go
+++ generator.go
@@ -1,5 +1,7 @@
 package main
 
+import "sort"
+
 type Rend interface {
 	Render(char rune) float64
 }
@@ -8,19 +10,40 @@ type Generator struct {
 	Renderer Rend
 }
 
-func New(renderer Rend) Generator {
+func NewGenerator(renderer Rend) Generator {
 	return Generator{
 		Renderer: renderer,
 	}
 }
 
 func (g *Generator) Generate(charset []rune, mode Mode) string {
-	var luminosities map[rune]float64
+	c := make([]rune, len(charset))
+	copy(c, charset)
+
+	luminosities := g.calculateLuminosities(c)
+	g.applyMode(c, luminosities, mode)
+
+	return string(c)
+}
+
+func (g *Generator) calculateLuminosities(charset []rune) map[rune]float64 {
+	luminosities := make(map[rune]float64)
 
 	for _, char := range charset {
 		luminosity := g.Renderer.Render(char)
 		luminosities[char] = luminosity
 	}
 
-	return ""
+	return luminosities
+}
+
+func (g *Generator) applyMode(charset []rune, luminosities map[rune]float64, mode Mode) {
+	sort.Slice(charset, func(i, j int) bool {
+		switch mode {
+		default:
+			return luminosities[charset[i]] < luminosities[charset[j]]
+		case Darkening:
+			return luminosities[charset[i]] > luminosities[charset[j]]
+		}
+	})
 }
